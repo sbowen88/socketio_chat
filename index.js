14f6c76531480ce7f14193ab2dff2957366a7acd
sbowen88 <sbowey@gmail.com>
2019-02-14 16:15:49 -0800

    message appears

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
   });
-});
 
 http.listen(3000, function() {
   console.log("listening on *:3000");
